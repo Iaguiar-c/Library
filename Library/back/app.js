dd44daa06c4a588d90eff4ff44807f1180a87cdf
IsabellaCorreiaAguiar <bellacorreia2004@gmail.com>
2024-03-10 21:21:49 -0300

    Delete, Update, Logout
    Delete, Update, Logout

--- Library/back/app.js
+++ Library/back/app.js
@@ -3,15 +3,16 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bcrypt = require("bcrypt");
 const jwt = require("jsonwebtoken");
-const nodemailer = require('nodemailer');
-const sgTransport = require('nodemailer-sendgrid-transport');
-const crypto = require('crypto');
+const nodemailer = require("nodemailer");
+const sgTransport = require("nodemailer-sendgrid-transport");
+const crypto = require("crypto");
 const Schema = mongoose.Schema;
 
 const transporter = nodemailer.createTransport(
   sgTransport({
     auth: {
-      api_key: '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',
+      api_key:
+        "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",
     },
   })
 );
@@ -19,7 +20,7 @@ const transporter = nodemailer.createTransport(
 const resetTokenSchema = new Schema({
   user: {
     type: Schema.Types.ObjectId,
-    ref: 'User', 
+    ref: "User",
   },
   token: {
     type: String,
@@ -31,7 +32,7 @@ const resetTokenSchema = new Schema({
   },
 });
 
-const ResetToken = mongoose.model('ResetToken', resetTokenSchema);
+const ResetToken = mongoose.model("ResetToken", resetTokenSchema);
 const app = express();
 
 //Config JSON response
@@ -47,37 +48,35 @@ app.get("/", (req, res) => {
 
 //Rota Privada
 app.get("/user/:id", checkToken, async (req, res) => {
-    const id = req.params.id
+  const id = req.params.id;
 
-    //verificar se o usuario existe
-    const user = await User.findById(id, '-password')
+  //verificar se o usuario existe
+  const user = await User.findById(id, "-password");
 
-    if(!user){
-        return res.status(404).json({msg: "Usuário não encontrado!"})
-    } 
+  if (!user) {
+    return res.status(404).json({ msg: "Usuário não encontrado!" });
+  }
 
-    res.status(200).json({user})
-})
+  res.status(200).json({ user });
+});
 
 function checkToken(req, res, next) {
-    const authHeader = req.headers['authorization']
-    const token = authHeader && authHeader.split(" ")[1]
+  const authHeader = req.headers["authorization"];
+  const token = authHeader && authHeader.split(" ")[1];
 
-    if(!token) {
-        return res.status(401).json({msg: "Acesso negado!"})
-    }
+  if (!token) {
+    return res.status(401).json({ msg: "Acesso negado!" });
+  }
 
-    try {
+  try {
+    const secret = process.env.secret;
 
-        const secret = process.env.secret
+    jwt.verify(token, secret);
 
-        jwt.verify(token, secret)
-
-        next()
-        
-    } catch (error) {
-        res.status(400).json({msg: "Token inválido!"})
-    }
+    next();
+  } catch (error) {
+    res.status(400).json({ msg: "Token inválido!" });
+  }
 }
 
 //Registrar usuario
@@ -163,48 +162,143 @@ app.post("/auth/login", async (req, res) => {
       {
         id: user._id,
       },
-      secret,
+      secret
     );
 
-    res.status(200).json({msg: "Usuário autenticado!", token})
+    res.status(200).json({ msg: "Usuário autenticado!", token });
   } catch (error) {
     console.log(error);
     res.status(500).json({ msg: "Erro no servidor" });
   }
 });
 
+// Logout User
+app.post("/auth/logout/:Id", async (req, res) => {
+  const Id = req.params.Id;
+
+  try {
+    // Verificar se o usuário existe antes de realizar o logout
+    const user = await User.findById(Id);
+    if (!user) {
+      return res.status(404).json({ msg: "Usuário não encontrado" });
+    }
+    
+    //Deslogar o usuário
+    res.status(200).json({ msg: "Usuário desconectado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Erro no servidor" });
+  }
+});
+
+//Update User
+app.put("/auth/update/:Id", async (req, res) => {
+  const Id = req.params.Id;
+  const { name, email, password, confirmpassword } = req.body;
+
+  // Validação dos campos
+  if (!name) {
+    return res.status(422).json({ msg: "Nome obrigatório" });
+  }
+
+  if (!email) {
+    return res.status(422).json({ msg: "Email obrigatório" });
+  }
+
+  if (!password) {
+    return res.status(422).json({ msg: "Senha obrigatória" });
+  }
+
+  // Verificar se o usuário existe antes de tentar atualizar
+  try {
+    const user = await User.findById(Id);
+    if (!user) {
+      return res.status(404).json({ msg: "Usuário não encontrado" });
+    }
+  } catch (error) {
+    console.log(error);
+    return res.status(500).json({ msg: "Erro no servidor" });
+  }
+
+  // Lógica para atualizar o usuário
+  try {
+    // Atualizar apenas os campos que foram fornecidos
+    const updatedFields = {};
+    if (name) updatedFields.name = name;
+    if (email) updatedFields.email = email;
+    if (password) {
+      // Validar se a senha e a confirmação são iguais
+      //Não esquecer de passar no Body-->raw ambos os campos password e confirmpassword
+      if (password !== confirmpassword) {
+        return res.status(422).json({ msg: "As senhas precisam ser iguais!" });
+      }
+      const salt = await bcrypt.genSalt(12);
+      const passwordHash = await bcrypt.hash(password, salt);
+      updatedFields.password = passwordHash;
+    }
+
+    //Atualizando o usuário
+    await User.findByIdAndUpdate(Id, updatedFields);
+
+    res.status(200).json({ msg: "Usuário atualizado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Erro no servidor" });
+  }
+});
+
+//Delete User
+app.delete("/auth/delete/:Id", async (req, res) => {
+  const Id = req.params.Id;
+
+  try {
+    // Verificar se o usuário existe antes de tentar deletar
+    const user = await User.findById(Id);
+    if (!user) {
+      return res.status(404).json({ msg: "Usuário não encontrado" });
+    }
+
+    // Lógica para que o usuário seja deletado
+    await User.findByIdAndDelete(Id);
+
+    res.status(200).json({ msg: "Usuário deletado com sucesso!" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ msg: "Erro no servidor" });
+  }
+});
 
 // TESTEEEEEE
 // Rota para solicitar a redefinição de senha
-app.post('/forgot-password', async (req, res) => {
+app.post("/forgot-password", async (req, res) => {
   const { email } = req.body;
 
   // Verifique se o usuário com o e-mail fornecido existe
   const user = await User.findOne({ email });
 
   if (!user) {
-      return res.status(404).json({ msg: 'Usuário não encontrado' });
+    return res.status(404).json({ msg: "Usuário não encontrado" });
   }
 
   // Crie um token de redefinição de senha
-  const resetToken = crypto.randomBytes(20).toString('hex');
-  
+  const resetToken = crypto.randomBytes(20).toString("hex");
+
   // Salve o token no banco de dados associado ao usuário
   const resetTokenObj = new ResetToken({
-      user: user._id,
-      token: resetToken,
-      expires: Date.now() + 3600000, // Token expira em 1 hora
+    user: user._id,
+    token: resetToken,
+    expires: Date.now() + 3600000, // Token expira em 1 hora
   });
 
   await resetTokenObj.save();
 
   // Envie o e-mail com o link de redefinição de senha usando NodeMailer
   const resetLink = `https://seusite.com/reset-password?token=${resetToken}`;
-  
+
   const mailOptions = {
-    from: 'isaaholiveira15@gmail.com',
-    to: 'kmulti2020@gmail.com',
-    subject: 'Recuperação de Senha',
+    from: "isaaholiveira15@gmail.com",
+    to: "kmulti2020@gmail.com",
+    subject: "Recuperação de Senha",
     html: `<p>Olá ${user.name},</p>
            <p>Clique no link abaixo para redefinir sua senha:</p>
            <a href="${resetLink}">${resetLink}</a>`,
@@ -213,30 +307,34 @@ app.post('/forgot-password', async (req, res) => {
   transporter.sendMail(mailOptions, (error, info) => {
     if (error) {
       console.error(error);
-      return res.status(500).json({ msg: 'Erro ao enviar e-mail de recuperação de senha' });
+      return res
+        .status(500)
+        .json({ msg: "Erro ao enviar e-mail de recuperação de senha" });
     }
 
-    console.log('E-mail enviado:', info.response);
-    res.status(200).json({ msg: 'E-mail de redefinição de senha enviado com sucesso' });
+    console.log("E-mail enviado:", info.response);
+    res
+      .status(200)
+      .json({ msg: "E-mail de redefinição de senha enviado com sucesso" });
   });
 });
 
 // Rota para atualizar a senha com base no token
-app.post('/reset-password', async (req, res) => {
+app.post("/reset-password", async (req, res) => {
   const { token, newPassword } = req.body;
 
   // Encontre o token no banco de dados
   const resetTokenObj = await ResetToken.findOne({ token });
 
   if (!resetTokenObj || resetTokenObj.expires < Date.now()) {
-      return res.status(400).json({ msg: 'Token inválido ou expirado' });
+    return res.status(400).json({ msg: "Token inválido ou expirado" });
   }
 
   // Encontre o usuário associado ao token
   const user = await User.findById(resetTokenObj.user);
 
   if (!user) {
-      return res.status(404).json({ msg: 'Usuário não encontrado' });
+    return res.status(404).json({ msg: "Usuário não encontrado" });
   }
 
   // Atualize a senha do usuário
@@ -248,9 +346,8 @@ app.post('/reset-password', async (req, res) => {
   await user.save();
   await resetTokenObj.remove();
 
-  res.status(200).json({ msg: 'Senha atualizada com sucesso' });
+  res.status(200).json({ msg: "Senha atualizada com sucesso" });
 });
-
 
 //Credenciais
 const dbUser = process.env.DB_USER;
