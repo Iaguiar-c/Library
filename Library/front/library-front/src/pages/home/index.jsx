2074616f2fdd3f3f5832ad7944cd09fae23313a5
IsabellaCorreiaAguiar <bellacorreia2004@gmail.com>
2024-05-01 19:47:43 -0300

    Implementando Modal com entradas

--- Library/front/library-front/src/pages/home/index.jsx
+++ Library/front/library-front/src/pages/home/index.jsx
@@ -2,13 +2,12 @@ import React, { useState } from "react";
 import { useTraducao } from "../../contextos/TraducaoProvider/TraducaoProvider";
 import { useTranslation } from "react-i18next";
 import { useAutenticacao } from "../../contextos/AutenticacaoProvider/AutenticacaoProvider";
-import { useLivros } from "../../contextos/LivrosProvider/LivrosProvider";
-import axios from "axios";
-import Card from "../../components/Cards";
-import GoogleBooksModal from "../../components/Modals/add-book-google-modal";
+//import { useLivros } from "../../contextos/LivrosProvider/LivrosProvider";
+//import axios from "axios";
+import SelectModal from "../../components/Modals/add-books-modal";
 
 const Home = () => {
-  const [isModalOpen, setIsModalOpen] = useState(false); // Estado para controlar a visibilidade do modal
+  const [isModalOpen, setIsModalOpen] = useState(false); 
   const { toggleTraducao } = useTraducao();
   const { t } = useTranslation();
   const { usuario } = useAutenticacao();
@@ -42,11 +41,11 @@ const Home = () => {
           className="block text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center"
           onClick={openModal}
         >
-          Adicionar com Google Livros
+          Adicionar Livros
         </button>
 
         {isModalOpen && (
-          <GoogleBooksModal isOpen={isModalOpen} onClose={closeModal} />
+          <SelectModal isOpen={isModalOpen} onClose={closeModal} />
         )}
       </div>
     </>
