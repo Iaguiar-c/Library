bcabd10195f054e0fbd3900c2c533cf886bb435c
IsabellaCorreiaAguiar <bellacorreia2004@gmail.com>
2024-05-08 14:05:16 -0300

    Correção - Tela Login - Bloco Senha

--- Library/front/library-front/src/components/PasswordField/PasswordField.jsx
+++ Library/front/library-front/src/components/PasswordField/PasswordField.jsx
@@ -1,51 +1,45 @@
-import { Visibility, VisibilityOff } from '@mui/icons-material';
-import { IconButton, InputAdornment, TextField } from '@mui/material';
 import React, { useState } from 'react';
+import { Visibility, VisibilityOff } from '@mui/icons-material';
+import { IconButton, InputAdornment } from '@mui/material';
+import { useTranslation } from 'react-i18next';
 
-const PasswordField = ({
-  passwordRef,
-  id = 'password',
-  label = 'Password',
-}) => {
+const PasswordField = ({ passwordRef, id = 'password' }) => {
+  const { t } = useTranslation();
   const [showPassword, setShowPassword] = useState(false);
 
-  const handleClick = () => {
+  const handleClickShowPassword = () => {
     setShowPassword(!showPassword);
-  };
-
-  const handleMouseDown = (e) => {
-    e.preventDefault();
   };
 
   return (
     <div>
       <label
         htmlFor={id}
-        className="block text-sm font-medium leading-6 text-gray-900"
+        className="block text-sm font-medium leading-6 text-primary-950"
       >
-        {label}
+        {t('senha')}
       </label>
-      <TextField
-        autoFocus
-        margin="normal"
-        variant="standard"
-        id={id}
-        type={showPassword ? 'text' : 'password'}
-        fullWidth
-        inputRef={passwordRef}
-        inputProps={{ minLength: 6 }}
-        required
-        InputProps={{
-          endAdornment: (
-            <InputAdornment position="end">
-              <IconButton onClick={handleClick} onMouseDown={handleMouseDown}>
-                {showPassword ? <VisibilityOff /> : <Visibility />}
-              </IconButton>
-            </InputAdornment>
-          ),
-        }}
-        className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-      />
+      <div className="relative">
+        <input
+          autoFocus
+          id={id}
+          type={showPassword ? 'text' : 'password'}
+          ref={passwordRef}
+          minLength={6}
+          required
+          className="block w-full rounded-md border-0 py-1.5 pr-10 text-primary-900 shadow-sm ring-1 ring-inset ring-primary-300 placeholder-text-primary-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+        />
+        <div className="absolute inset-y-0 right-0 flex items-center pr-2">
+          <IconButton
+            onClick={handleClickShowPassword}
+            size="small"
+            className="p-1"
+            style={{ marginLeft: 'auto' }}
+          >
+            {showPassword ? <VisibilityOff /> : <Visibility />}
+          </IconButton>
+        </div>
+      </div>
     </div>
   );
 };
