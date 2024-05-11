7928a4880aded0ca4be6df21336af4a6eb665e69
IsabellaCorreiaAguiar <bellacorreia2004@gmail.com>
2024-05-11 20:30:35 -0300

    fix bug

--- Library/front/library-front/src/components/Modals/add-books-modal.jsx
+++ Library/front/library-front/src/components/Modals/add-books-modal.jsx
@@ -1,9 +1,14 @@
 import React, { useRef, useEffect, useState } from "react";
+import { useAutenticacao } from "../../contextos/AutenticacaoProvider/AutenticacaoProvider";
+import { Api } from "../../services/api";
+import axios from "axios";
 
 const ModalForm = ({ isOpen, onClose, book }) => {
   const modalRef = useRef();
   const [modalOpen, setModalOpen] = useState(false);
   const [manualEntry, setManualEntry] = useState(false);
+  const [imageUrl, setImageUrl] = useState("");
+  const { usuario, token, config } = useAutenticacao();
 
   useEffect(() => {
     setModalOpen(isOpen);
@@ -38,6 +43,42 @@ const ModalForm = ({ isOpen, onClose, book }) => {
     const imageUrl = event.target.value;
 
     setManualEntry(imageUrl !== "");
+  };
+
+  const handleSubmit = async (event) => {
+    event.preventDefault();
+
+    if (!token || !usuario) {
+      console.error(
+        "Token ou usuário não disponível. Realize o login novamente."
+      );
+      return;
+    }
+
+    const publicationYear = parseInt(
+      book?.volumeInfo?.publishedDate?.substring(0, 4)
+    );
+
+    const data = {
+      title: book?.volumeInfo?.title || "",
+      author: book?.volumeInfo?.authors?.join(", ") || "",
+      publicationYear: publicationYear || 0,
+      category: book?.volumeInfo?.categories?.join(", ") || "",
+      description: book?.volumeInfo?.description || "",
+      imageURL: imageUrl,
+      status: "available",
+      userId: usuario?._id,
+    };
+
+    try {
+      const response = await Api.post("/books/create", data, config);
+      onClose();
+    } catch (error) {
+      console.error("Erro ao criar livro:", error.message);
+      if (error.response) {
+        console.error("Detalhes do erro:", error.response.data);
+      }
+    }
   };
 
   if (!modalOpen) return null;
@@ -173,17 +214,13 @@ const ModalForm = ({ isOpen, onClose, book }) => {
                     Categoria
                   </label>
                   <div className="mt-2">
-                    <select
+                    <input
                       id="categoria"
                       name="categoria"
                       autoComplete="categoria"
                       className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
                       value={book?.volumeInfo?.categories || ""}
-                    >
-                      <option>--</option>
-                      <option>bla</option>
-                      <option>bla</option>
-                    </select>
+                    ></input>
                   </div>
                 </div>
 
@@ -208,7 +245,7 @@ const ModalForm = ({ isOpen, onClose, book }) => {
                 {!manualEntry && (
                   <div className="sm:col-span-3">
                     <label
-                      htmlFor="imagem"
+                      htmlFor="imagemURL"
                       className="block text-sm font-medium leading-6 text-gray-900"
                     >
                       Capa do Livro
@@ -238,6 +275,7 @@ const ModalForm = ({ isOpen, onClose, book }) => {
             </button>
             <button
               type="submit"
+              onClick={handleSubmit}
               className="rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Adicionar
