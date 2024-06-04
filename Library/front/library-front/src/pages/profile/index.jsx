83e31627d2abaa5525fdfb618e983c09a764d371
IsabellaCorreiaAguiar <bellacorreia2004@gmail.com>
2024-06-04 01:10:08 -0300

    fix: Profile design

--- Library/front/library-front/src/pages/profile/index.jsx
+++ Library/front/library-front/src/pages/profile/index.jsx
@@ -43,9 +43,18 @@ export default function Profile() {
     setError(null);
 
     try {
-      await editUsuario(usuario._id, { name: username, email: email, profile: profile });
+      await editUsuario(usuario._id, {
+        name: username,
+        email: email,
+        profile: profile,
+      });
 
-      setUsuario((prev) => ({ ...prev, name: username, email: email,  profile: profile }));
+      setUsuario((prev) => ({
+        ...prev,
+        name: username,
+        email: email,
+        profile: profile,
+      }));
 
       enqueueSnackbar("Seu usuário foi alterado com sucesso.", {
         variant: "success",
@@ -54,8 +63,10 @@ export default function Profile() {
     } catch (error) {
       console.error(error);
 
-      const errorMessage = error.response?.data?.error || 'Não foi possível editar o usuário. Por favor, tente novamente.';
-      enqueueSnackbar(errorMessage, { variant: 'error' });
+      const errorMessage =
+        error.response?.data?.error ||
+        "Não foi possível editar o usuário. Por favor, tente novamente.";
+      enqueueSnackbar(errorMessage, { variant: "error" });
     } finally {
       setLoading(false);
     }
@@ -82,46 +93,52 @@ export default function Profile() {
     }
   };
 
-	if (!usuario) {
-		window.location.pathname = "/login";
-	}
+  if (!usuario) {
+    window.location.pathname = "/login";
+  }
 
   return (
     <>
-      <div className="max-w-md mx-auto bg-white shadow-md rounded-lg overflow-hidden">
-        {usuario && (
-          <div className="p-6">
-            <div className="flex items-center justify-center">
-              <img
-                src={profileUrl || ""}
-                alt="Avatar"
-                className="h-20 w-20 rounded-full object-cover"
-              />
-            </div>
-            <h2 className="text-2xl font-semibold text-gray-800 mt-4">
-              {usuario.name}
-              <span className="ml-2">
-                <FontAwesomeIcon
-                  icon={faEdit}
-                  className="text-gray-500 cursor-pointer"
-                  onClick={() => setShowEditModal(true)}
-                />
-                <span className="ml-2">
-                  <FontAwesomeIcon
-                    icon={faTrash}
-                    className="text-red-500 cursor-pointer"
-                    onClick={() => setShowDeleteModal(true)}
-                  />
-                </span>
-              </span>
-            </h2>
-            <p className="text-gray-600 mt-2">{usuario.email}</p>
+     <div className="flex justify-center items-center h-screen">
+  <div className="max-w-3xl w-full mx-auto bg-primary-100 shadow-md rounded-lg overflow-hidden">
+    {usuario && (
+      <div className="p-8">
+        <div className="flex items-center justify-between mb-4">
+          <h1 className="text-4xl font-semibold text-primary-950 m-6">
+            Seu Perfil
+          </h1>
+          <div className="flex items-center">
+            <FontAwesomeIcon
+              icon={faEdit}
+              className="text-primary-700 cursor-pointer mr-4 text-2xl"
+              onClick={() => setShowEditModal(true)}
+            />
+            <FontAwesomeIcon
+              icon={faTrash}
+              className="text-primary-700 cursor-pointer text-2xl"
+              onClick={() => setShowDeleteModal(true)}
+            />
           </div>
-        )}
-
+        </div>
+        <div className="flex items-center mb-6">
+          <img
+            src={profileUrl || ""}
+            alt="Avatar"
+            className="h-50 w-48 rounded-full object-cover mr-6"
+          />
+          <div>
+            <h2 className="text-3xl font-semibold text-primary-950">
+              Olá, {usuario.name}!
+            </h2>
+            <p className="text-xl text-primary-900 mt-2">{usuario.email}</p>
+          </div>
+        </div>
+      </div>
+    )}
+  </div>
         {!usuario && (
           <div className="p-6">
-            <p className="text-gray-600">
+            <p className="text-gray-900">
               Faça login para visualizar o perfil.
             </p>
           </div>
@@ -178,7 +195,7 @@ export default function Profile() {
                     htmlFor="profile"
                     className="block mb-2 text-sm font-medium text-primary-950 dark:text-primary"
                   >
-                    Foto de Perfil 
+                    Foto de Perfil
                   </label>
                   <input
                     type="text"
