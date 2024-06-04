23c691e64071884aa3325ed8e56b71db450f3e71
IsabellaCorreiaAguiar <bellacorreia2004@gmail.com>
2024-06-04 02:07:43 -0300

    fix: editProfile desing e correção de cores do header

--- Library/front/library-front/src/pages/profile/index.jsx
+++ Library/front/library-front/src/pages/profile/index.jsx
@@ -16,6 +16,7 @@ export default function Profile() {
   const [error, setError] = useState(null);
   const [loading, setLoading] = useState(false);
   const { deleteUsuario, editUsuario } = useUsuario();
+  const [isOpen, setIsOpen] = useState(false);
   const navigate = useNavigate();
   const [profileUrl, setProfileUrl] = useState(LogoPadrao);
   const [showDeleteModal, setShowDeleteModal] = useState(false);
@@ -70,6 +71,7 @@ export default function Profile() {
     } finally {
       setLoading(false);
     }
+    handleCloseEditModal();
   };
 
   const handleDeleteProfile = async () => {
@@ -97,48 +99,58 @@ export default function Profile() {
     window.location.pathname = "/login";
   }
 
+  const handleCloseEditModal = () => {
+    setIsOpen(false);
+  };
+
+  const handleEditClick = () => {
+    setIsOpen(true); // Abrir o modal de edição
+  };
+
   return (
     <>
-     <div className="flex justify-center items-center h-screen">
-  <div className="max-w-3xl w-full mx-auto bg-primary-100 shadow-md rounded-lg overflow-hidden">
-    {usuario && (
-      <div className="p-8">
-        <div className="flex items-center justify-between mb-4">
-          <h1 className="text-4xl font-semibold text-primary-950 m-6">
-            Seu Perfil
-          </h1>
-          <div className="flex items-center">
-            <FontAwesomeIcon
-              icon={faEdit}
-              className="text-primary-700 cursor-pointer mr-4 text-2xl"
-              onClick={() => setShowEditModal(true)}
-            />
-            <FontAwesomeIcon
-              icon={faTrash}
-              className="text-primary-700 cursor-pointer text-2xl"
-              onClick={() => setShowDeleteModal(true)}
-            />
-          </div>
+      <div className="flex justify-center items-center h-screen">
+        <div className="max-w-3xl w-full mx-auto bg-primary-100 shadow-md rounded-lg overflow-hidden">
+          {usuario && (
+            <div className="p-8">
+              <div className="flex items-center justify-between mb-4">
+                <h1 className="text-4xl font-semibold text-primary-950 m-6">
+                  Seu Perfil
+                </h1>
+                <div className="flex items-center">
+                  <FontAwesomeIcon
+                    icon={faEdit}
+                    className="text-primary-700 cursor-pointer mr-4 text-2xl"
+                    onClick={handleEditClick}
+                  />
+                  <FontAwesomeIcon
+                    icon={faTrash}
+                    className="text-primary-700 cursor-pointer text-2xl"
+                    onClick={() => setShowDeleteModal(true)}
+                  />
+                </div>
+              </div>
+              <div className="flex items-center mb-6">
+                <img
+                  src={profileUrl || ""}
+                  alt="Avatar"
+                  className="h-50 w-48 rounded-full object-cover mr-6"
+                />
+                <div>
+                  <h2 className="text-3xl font-semibold text-primary-950">
+                    Olá, {usuario.name}!
+                  </h2>
+                  <p className="text-xl text-primary-900 mt-2">
+                    {usuario.email}
+                  </p>
+                </div>
+              </div>
+            </div>
+          )}
         </div>
-        <div className="flex items-center mb-6">
-          <img
-            src={profileUrl || ""}
-            alt="Avatar"
-            className="h-50 w-48 rounded-full object-cover mr-6"
-          />
-          <div>
-            <h2 className="text-3xl font-semibold text-primary-950">
-              Olá, {usuario.name}!
-            </h2>
-            <p className="text-xl text-primary-900 mt-2">{usuario.email}</p>
-          </div>
-        </div>
-      </div>
-    )}
-  </div>
         {!usuario && (
           <div className="p-6">
-            <p className="text-gray-900">
+            <p className="text-primary-900">
               Faça login para visualizar o perfil.
             </p>
           </div>
@@ -150,75 +162,99 @@ export default function Profile() {
           onConfirm={handleDeleteProfile}
           isUserDelete={true}
         />
-        <ModalGenerico
-          isOpen={showEditModal}
-          onRequestClose={closeModal}
-          content={
-            <>
-              <form className="space-y-6" onSubmit={handleEditChangeSubmit}>
-                <div>
-                  <label
-                    htmlFor="username"
-                    className="block mb-2 text-sm font-medium text-primary-950 dark:text-primary"
-                  >
-                    Nome de usuário
-                  </label>
-                  <input
-                    type="text"
-                    name="username"
-                    value={username}
-                    onChange={(e) => setUsername(e.target.value)}
-                    placeholder="Editar nome de usuário"
-                    className="bg-primary-50 border border-primary-300 text-primary-950 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-primary-700 dark:border-primary-600 dark:placeholder-primary-400 dark:text-primary dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    required
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="email"
-                    className="block mb-2 text-sm font-medium text-primary-950 dark:text-primary"
-                  >
-                    E-mail
-                  </label>
-                  <input
-                    type="text"
-                    name="email"
-                    value={email}
-                    onChange={(e) => setEmail(e.target.value)}
-                    placeholder="Editar e-mail"
-                    className="bg-primary-50 border border-primary-300 text-primary-950 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-primary-700 dark:border-primary-600 dark:placeholder-primary-400 dark:text-primary dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    required
-                  />
-                </div>
-                <div>
-                  <label
-                    htmlFor="profile"
-                    className="block mb-2 text-sm font-medium text-primary-950 dark:text-primary"
-                  >
-                    Foto de Perfil
-                  </label>
-                  <input
-                    type="text"
-                    name="profile"
-                    value={profile}
-                    onChange={(e) => setProfile(e.target.value)}
-                    placeholder="Editar e-mail"
-                    className="bg-primary-50 border border-primary-300 text-primary-950 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-primary-700 dark:border-primary-600 dark:placeholder-primary-400 dark:text-primary dark:focus:ring-primary-500 dark:focus:border-primary-500"
-                    required
-                  />
-                </div>
-                <div>
-                  <button
-                    type="submit"
-                    className="w-full bg-primary-700 py-2 px-4 rounded-md text-sm font-semibold text-white shadow-sm hover:bg-primary-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-700"
-                  >
-                    Editar
-                  </button>
-                </div>
-              </form>
-            </>
-          }
-        />
+
+{isOpen && (
+  <div className="fixed inset-0 z-50 flex items-center justify-center bg-primary-950 bg-opacity-30">
+    
+    <div className="p-8 bg-primary-100 shadow-md rounded-lg" style={{ width: '50%' }}>
+      <div className="flex justify-between mb-4">
+        <h1 className="text-2xl font-semibold text-primary-950">
+          Editar Perfil
+        </h1>
+        <button
+          onClick={handleCloseEditModal}
+          className="text-primary-700 hover:text-primary-900 focus:outline-none"
+        >
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            className="h-6 w-6"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke="currentColor"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth={2}
+              d="M6 18L18 6M6 6l12 12"
+            />
+          </svg>
+        </button>
+      </div>
+      <form className="space-y-4" onSubmit={handleEditChangeSubmit}>
+        <div>
+          <label
+            htmlFor="username"
+            className="block mb-1 text-sm font-medium text-primary-950"
+          >
+            Nome de usuário
+          </label>
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Editar nome de usuário"
+            className="bg-primary-50 border border-primary-300 text-primary-950 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="email"
+            className="block mb-1 text-sm font-medium text-primary-950"
+          >
+            E-mail
+          </label>
+          <input
+            type="text"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Editar e-mail"
+            className="bg-primary-50 border border-primary-300 text-primary-950 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"
+            required
+          />
+        </div>
+        <div>
+          <label
+            htmlFor="profile"
+            className="block mb-1 text-sm font-medium text-primary-950"
+          >
+            Foto de Perfil
+          </label>
+          <input
+            type="text"
+            name="profile"
+            value={profile}
+            onChange={(e) => setProfile(e.target.value)}
+            placeholder="Editar e-mail"
+            className="bg-primary-50 border border-primary-300 text-primary-950 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-1.5"
+            required
+          />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="w-full bg-primary-700 py-2 px-4 rounded-md text-sm font-semibold text-primary-100 shadow-sm hover:bg-primary-700 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-primary-700"
+          >
+            Editar
+          </button>
+        </div>
+      </form>
+    </div>
+  </div>
+)}
       </div>
     </>
   );
