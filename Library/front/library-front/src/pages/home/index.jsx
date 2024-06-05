7c50d57d447372914b144fa413e829ddd701d35a
IsabellaCorreiaAguiar <bellacorreia2004@gmail.com>
2024-06-04 23:22:35 -0300

    fix: visualização botão 'Carregar Mais'

--- Library/front/library-front/src/pages/home/index.jsx
+++ Library/front/library-front/src/pages/home/index.jsx
@@ -224,30 +224,30 @@ const Home = () => {
         />
       )}
       <div className="m-8">
-        {filteredLivros.length > 0 ? (
-          <>
-            {viewMode === "card" ? (
-              <BooksCard books={filteredLivros} livroCovers={livroCovers} />
-            ) : (
-              <BooksTable books={filteredLivros} />
-            )}
-            {currentPage < totalPages && (
-              <div className="flex justify-center mt-4">
-                <button
-                  className="bg-primary-700 text-white font-medium rounded-lg text-sm px-5 py-2.5"
-                  onClick={loadMoreBooks}
-                >
-                  Carregar Mais
-                </button>
-              </div>
-            )}
-          </>
-        ) : (
-          <p className="text-3xl text-center mt-20 text-primary-950 font-bold">
-            {t("voce_ainda_nao_possuem_livros_adicionados")}
-          </p>
-        )}
-      </div>
+  {filteredLivros.length > 0 ? (
+    <>
+      {viewMode === "card" ? (
+        <BooksCard books={filteredLivros} livroCovers={livroCovers} />
+      ) : (
+        <BooksTable books={filteredLivros} />
+      )}
+      {viewMode !== "table" && currentPage < totalPages && (
+        <div className="flex justify-center mt-4">
+          <button
+            className="bg-primary-700 text-white font-medium rounded-lg text-sm px-5 py-2.5"
+            onClick={loadMoreBooks}
+          >
+            Carregar Mais
+          </button>
+        </div>
+      )}
+    </>
+  ) : (
+    <p className="text-3xl text-center mt-20 text-primary-950 font-bold">
+      {t("voce_ainda_nao_possuem_livros_adicionados")}
+    </p>
+  )}
+</div>
       <Notification
         message={notification.message}
         variant={notification.variant}
